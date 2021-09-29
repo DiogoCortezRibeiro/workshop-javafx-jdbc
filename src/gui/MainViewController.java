6b2ed7fc6b06fcc5da243cd0cc57ba96b005f59d
Diogo Cortez <diogocortezr@gmail.com>
2021-09-28 21:40:20 -0300

    Create controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,45 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+	
+	@FXML
+	private MenuItem menuItemVendedor;
+	
+	@FXML
+	private MenuItem menuItemDepartamento;
+	
+	@FXML
+	private MenuItem menuItemAjuda;
+	
+	@FXML
+	public void onMenuItemVendedorAction()
+	{
+		System.out.println("Menu item vendedor");
+	}
+	
+	@FXML
+	public void onMenuItemDepartamentoAction()
+	{
+		System.out.println("Menu item departamento");
+	}
+	
+	@FXML
+	public void onMenuItemSaidaAction()
+	{
+		System.out.println("Menu item saida");
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
