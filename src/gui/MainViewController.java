7e647b9c10839c8a2d4a938c6ac5e585061eac41
Diogo Cortez <diogocortezr@gmail.com>
2021-09-29 20:48:08 -0300

    Departamento lista design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -36,7 +36,7 @@ public class MainViewController implements Initializable {
 	@FXML
 	public void onMenuItemDepartamentoAction()
 	{
-		System.out.println("Menu item departamento");
+		loadView("/gui/DepartamentoLista.fxml");
 	}
 	
 	@FXML
