325f94866f55dc2891f52dbbd3fa7638a23958b0
Diogo Cortez <diogocortezr@gmail.com>
2021-09-29 21:04:31 -0300

    Departamento lista controller

---
+++ src/gui/DepartamentoListaController.java
@@ -0,0 +1,49 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Departamento;
+
+public class DepartamentoListaController implements Initializable {
+
+	@FXML
+	private TableView<Departamento> tableViewDepartamento;
+	
+	@FXML
+	private TableColumn<Departamento, Integer> tableColumId;
+	
+	@FXML 
+	private TableColumn<Departamento, String> tableColumNome;
+	
+	@FXML
+	private Button btNovo;
+	
+	@FXML
+	public void onBtNovoAction()
+	{
+		System.out.println("On Bt Novo action");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartamento.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+}
